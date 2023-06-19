12ea49e92809526c5936865a33ecca5e65469184
Vlad Akhpanov <0whiterthanwhite0@gmail.com>
2023-06-19 23:41:32 +0400

    Initial Commit

---
+++ Homepwner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homepwner
+//
+//  Created by Vlad Akhpanov on 19.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
