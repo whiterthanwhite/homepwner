fcaa7d3bde37ae5ca70ea8647f3b91d435885a6a
Vlad Akhpanov <0whiterthanwhite0@gmail.com>
2023-06-21 23:14:26 +0400

    added dynamic sell height and dynamic types to labels

--- Homepwner/ItemCell.swift
+++ Homepwner/ItemCell.swift
@@ -11,4 +11,12 @@ class ItemCell: UITableViewCell {
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var serialNumberLabel: UILabel!
     @IBOutlet var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+    }
 }
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -21,7 +21,9 @@ class ItemsViewController: UITableViewController {
         
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
-        tableView.rowHeight = 65
+        // tableView.rowHeight = 65
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 65
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
